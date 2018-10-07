7dcdf5a61e456e49586047b28fb06c0a73a8bdde
Chris Lacaille <chrisl@enableip.com>
2018-10-07 05:18:22 -0500

    Code Cleanup

--- dev/connect.py
+++ dev/connect.py
@@ -14,8 +14,7 @@ def get_columns(sheet_dict, column_dict={}):
 def get_site_names(sheet_dict, site_list=[]):
     for row in sheet_dict['rows']:
         for row_cell in row['cells']:
-            # TODO: find_siteName_column_id() function
-            # find_siteName_column_id(sheet_dict)
+            # TODO: create function that searches for ID of for "Site Name" column
             if row_cell['columnId'] == 5562647857915780 and "displayValue" in row_cell.keys():
                 site_list.append(row_cell['displayValue'])
     return site_list
@@ -30,20 +29,7 @@ solis_test_sheet_id = 3073042256553860
 alleyton_test_sheet_id = 5850406480832388
 # Retrieve Smartsheet: "TEST - Alleyton Inventory"
 current_sheet = ss_client.Sheets.get_sheet(alleyton_test_sheet_id)
-
-# TODO: test dictionary object for function testing
 custom_sheet_dict = current_sheet.to_dict()
 
-
-''' 
-MODEL FOR UPDATING SMARTSHEET
-
-updated_sheet = ss_client.Sheets.update_sheet(
-    solis_test_sheet_id,       # sheet_id
-    ss_client.models.Sheet({
-        'name': '--New Name--'
-    })
-)
-
-'''
-
+# get_columns(custom_sheet_dict)
+# get_site_names(custom_sheet_dict)
