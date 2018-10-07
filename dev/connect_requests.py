7ae70298858099720a2ba58b4cc1eafe4a64bbda
Chris Lacaille <ChrisL108@users.noreply.github.com>
2018-10-07 05:07:32 -0500

    Delete connect_requests.py
    requests library not necessary at this time.

--- dev/connect_requests.py
+++
@@ -1,9 +0,0 @@
-import json, requests, pandas as pd
-from config import ss_user
-
-
-solis_test_sheet_id = 3073042256553860
-
-ss_url = "https://api.smartsheet.com/2.0/sheets"
-ss_auth = f"Bearer {ss_user["access_token"]}"
-ss_cont_type = "application/json"
