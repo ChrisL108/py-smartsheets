416eddde750b51d558cb7e30a97980af1e59843f
Chris Lacaille <chrisl@enableip.com>
2018-10-07 23:57:46 -0500

    add get_sheet_column_id_wan_ip(sheet_dict)

--- dev/connect.py
+++ dev/connect.py
@@ -1,10 +1,18 @@
-import smartsheet
+import smartsheet, json
 from config import *
-# import pandas as pd, json
 
 
 # Returns a dict of all columns: { 'id'(key) : 'title'(value) }
-def get_column_dict(sheet_dict, column_dict={}):
+from dev.config import alleyton_test_sheet_id, ss_user
+
+
+def get_sheet_columns(sheet_dict, column_dict=None):
+    """
+    :type sheet_dict: dict
+    :type column_dict: dict
+    """
+    if column_dict is None:
+        column_dict = {}
     for column in sheet_dict['columns']:
         column_name = column['title']
         column_id = column['id']
@@ -13,20 +21,37 @@ def get_column_dict(sheet_dict, column_dict={}):
 
 
 # Returns the ID of Site Name column
-def get_site_name_id(sheet_dict):
+def get_sheet_column_id_site_name(sheet_dict):
     for column in sheet_dict['columns']:
         if 'site' and 'name' in column['title'].lower():
             return column['id']
 
 
+def get_sheet_column_id_wan_ip(sheet_dict):
+    for column in sheet_dict['columns']:
+        if 'wan' and 'customer'\
+                or 'wan' and 'gateway' in column['title'].lower():
+            return column['id']  # 2571976230365060
+
+
 # Returns a list of all Site Name values in a given sheet
-def get_site_name_list(sheet_dict, site_list=[]):
+def get_sheet_column_values_site_name(sheet_dict, site_list=[]):
     for row in sheet_dict['rows']:
         for row_cell in row['cells']:
-            sheet_site_name_id = get_site_name_id(sheet_dict)
+            sheet_site_name_id = get_sheet_column_id_site_name(sheet_dict)
             if row_cell['columnId'] == sheet_site_name_id and "displayValue" in row_cell.keys():
                 site_list.append(row_cell['displayValue'])
     return site_list
+
+
+# TODO: get_value()
+'''
+getValue( "Site", "Column Name", sheet): 
+    columnToFind(displayValue = "Column Name")
+    val = parse("Site Name")
+    if val[0] and val[1] in column["Site Name"]:
+        
+'''
 
 
 # Create a Smartsheet client object, available functions to this obj in API docs
@@ -35,15 +60,9 @@ ss_client = smartsheet.Smartsheet(ss_user['access_token'])
 ss_client.errors_as_exceptions(True)
 
 
-
 # Retrieve Smartsheet: "TEST - Alleyton Inventory"
-alleyton_test_sheet = ss_client.Sheets.get_sheet(alleyton_test_sheet_id)
-alleyton_sheet_dict = alleyton_test_sheet.to_dict()
+alleyton_sheet = ss_client.Sheets.get_sheet(alleyton_test_sheet_id)
+alleyton_sheet_dict = alleyton_sheet.to_dict()
 
-# get_column_dict(custom_sheet_dict)
-# get_site_name_list(custom_sheet_dict)
-
-'''
-    solis_sheet_id = 3073042256553860
-    alleyton_sheet_id = 5850406480832388
-'''
+#alleyton_columns = get_sheet_columns(alleyton_sheet_dict)
+#alleyton_sites = get_sheet_column_values_site_name(alleyton_sheet_dict)
